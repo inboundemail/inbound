18c4fe2874540354a6b06c2a4581890a72fa0fec
R44VC0RP <n01508660@unf.edu>
2025-05-27 20:08:14 -0400

    feat(auth): add trusted origins for authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,6 +9,7 @@ import * as schema from "./db/schema";
 const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export const auth = betterAuth({
+    trustedOrigins: [process.env.VERCEL_URL!],
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: schema
