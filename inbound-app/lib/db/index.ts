bd50be8cd320f2bf9a376d3a6e5f4623021f99df
Ryan Vogel <ryan@mandarin3d.com>
2025-08-06 20:51:12 -0400

    Update database connection configuration to use port 6432 for improved connectivity.

--- inbound-app/lib/db/index.ts
+++ inbound-app/lib/db/index.ts
@@ -6,6 +6,7 @@ const pool = new Pool({
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
+  port: 6432,
   database: process.env.DATABASE_NAME,
   ssl: {
     rejectUnauthorized: true,
